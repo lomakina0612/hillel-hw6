79147f0684fc13875dfe54b8eb455b14f3d3418e
Elena Lomakina <lomakina0612@gmail.com>
2024-08-21 14:03:01 +0200

    replace Yes/No with YES/NO

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ with open('student_performance_prediction.csv', mode='rt', newline='\n') as file
     csv_writer.writeheader()
     
     numeric_columns = ['Study Hours per Week', 'Attendance Rate', 'Previous Grades']
+    yes_no_columns = ['Participation in Extracurricular Activities', 'Passed']
 
     for row in csv_reader:
         try:
             for column in numeric_columns:
                 row[column] = int(float(row[column]) + 0.5)
-                
+            
+            for column in yes_no_columns:
+                row[column] = row[column].upper()
+             
             csv_writer.writerow(row)
         
         except ValueError:
