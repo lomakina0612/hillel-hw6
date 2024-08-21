66833d49bc172af3509e3c75f102958594f79da4
Elena Lomakina <lomakina0612@gmail.com>
2024-08-21 13:46:09 +0200

    add list numeric_columns

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 
 import csv
 
-# with open('student_performance_prediction.csv', mode='r', newline='\n') as file:
-#     csv_reader = csv.reader(file, delimiter=' ', quotechar='|')
-    
-#     for row in csv_reader:
-#         print(row)
-
-
 with open('student_performance_prediction.csv', mode='rt', newline='\n') as file, \
      open('new_student_performance_prediction.csv', mode='wt', newline='\n') as file_to_write:
     
-    csv_reader = csv.DictReader(file, delimiter=',', quotechar='|')
+    csv_reader = csv.DictReader(file, delimiter=',')
     fieldnames = csv_reader.fieldnames
     csv_writer = csv.DictWriter(file_to_write, fieldnames=fieldnames)
     
-    csv_writer.writeheader() 
+    csv_writer.writeheader()
     
+    numeric_columns = ['Study Hours per Week', 'Attendance Rate', 'Previous Grades']
 
     for row in csv_reader:
         try:
-        
-            row['Study Hours per Week'] = int(float(row['Study Hours per Week']) + 0.5)
-            row['Attendance Rate'] = int(float(row['Attendance Rate']) + 0.5)
-            row['Previous Grades'] = int(float(row['Previous Grades']) + 0.5)
-            
+            for column in numeric_columns:
+                row[column] = int(float(row[column]) + 0.5)
+                
             csv_writer.writerow(row)
-            
-        except Exception:
-            print(f"Line {row['Student ID']} contains gaps. Skipped")
         
+        except ValueError:
+            print(f"Line {row['Student ID']} contains gaps. Skipped")
+            
